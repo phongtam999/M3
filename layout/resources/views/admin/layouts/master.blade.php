cdc7b2f984863b157d74c6734c7b8a34acc9c4ce
phongtam999 <tamphongtrinh1991@gmail.com>
2022-12-05 16:14:14 +0700

    first commit

---
+++ layout/resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,28 @@
+
+@include('admin.includes.header')
+
+@include('admin.includes.sidebar')
+
+@yield('content')
+ {{-- @include('admin.includes.main') --}}
+
+@include('admin.includes.footer')
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('assets/vendor/chart.js/chart.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/echarts/echarts.min.js')}}"></script>
+  {{-- <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script> --}}
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
